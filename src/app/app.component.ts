b7786058d2bab202c57121ecfc7d2405d96d1cee
Shy Agam <shyagam@gmail.com>
2023-10-08 23:03:56 +0300

    🧹 Cleaned up demo scaffolding code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet],
   providers: [
     ScreenTrackingService,
     UserTrackingService
@@ -14,6 +13,4 @@ import { ScreenTrackingService, UserTrackingService } from '@angular/fire/analyt
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'herum-il';
-}
+export class AppComponent { }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,7 @@
 import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { provideAnalytics, getAnalytics } from '@angular/fire/analytics';
@@ -14,28 +16,29 @@ import { provideStorage, getStorage } from '@angular/fire/storage';
 
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
-import { provideServiceWorker } from '@angular/service-worker';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-    importProvidersFrom([
-        provideFirebaseApp(() => initializeApp(environment.firebase)),
-        provideAnalytics(() => getAnalytics()),
-        provideAuth(() => getAuth()),
-        provideDatabase(() => getDatabase()),
-        provideFirestore(() => getFirestore()),
-        provideFunctions(() => getFunctions()),
-        provideMessaging(() => getMessaging()),
-        providePerformance(() => getPerformance()),
-        provideRemoteConfig(() => getRemoteConfig()),
-        provideStorage(() => getStorage())
-    ]),
-    provideServiceWorker('ngsw-worker.js', {
-        enabled: !isDevMode(),
-        registrationStrategy: 'registerWhenStable:30000'
-    }),
-    provideAnimations()
-]
+    providers: [
+        provideRouter(routes),
+
+        importProvidersFrom([
+            provideFirebaseApp(() => initializeApp(environment.firebase)),
+            provideAnalytics(() => getAnalytics()),
+            provideAuth(() => getAuth()),
+            provideDatabase(() => getDatabase()),
+            provideFirestore(() => getFirestore()),
+            provideFunctions(() => getFunctions()),
+            provideMessaging(() => getMessaging()),
+            providePerformance(() => getPerformance()),
+            provideRemoteConfig(() => getRemoteConfig()),
+            provideStorage(() => getStorage())
+        ]),
+
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+        }),
+
+        provideAnimations()
+    ]
 };
