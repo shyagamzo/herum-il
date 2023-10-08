5734e69bbd33a6f35061ce75b189b5ed00921cba
Shy Agam <shyagam@gmail.com>
2023-10-08 23:11:44 +0300

    ✨ Generated a first component

---
+++ src/app/intention-selection/intention-selection.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-intention-selection',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './intention-selection.component.html',
+  styleUrls: ['./intention-selection.component.scss']
+})
+export default class IntentionSelectionComponent {
+
+}
