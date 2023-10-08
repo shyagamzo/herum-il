1ecd77c10978a635da8d5aadcc6073930fb9a4fc
Shy Agam <shyagam@gmail.com>
2023-10-08 23:22:45 +0300

    🔧 Configured intention selection component to be OnPush

--- src/app/intention-selection/intention-selection.component.ts
+++ src/app/intention-selection/intention-selection.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,7 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './intention-selection.component.html',
-  styleUrls: ['./intention-selection.component.scss']
+  styleUrls: ['./intention-selection.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class IntentionSelectionComponent {
 
